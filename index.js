cd9f77932cc11870e1b645d17f62686629577930
phuc nam <phucnammeowmeow@gmail.com>
2023-03-30 21:47:24 +0700

    add file index

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @format
+ */
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import App from './navigation/App';
+AppRegistry.registerComponent(appName,()=> ()=> <App/>); 
